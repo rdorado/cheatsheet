323d433e55fe25e0f1b0bf95951d02277321a4ca
Ruben Dario Dorado Sanchez <rdsanchez@cae.com>
2019-07-19 17:32:54 -0400

    Partially implemented Stanford pos tagger wrapper

--- src/java/org/rdorado/cheatsheet/core/wrapers/StanfordCoreNLPWrapper.java
+++ src/java/org/rdorado/cheatsheet/core/wrapers/StanfordCoreNLPWrapper.java
@@ -4,22 +4,25 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
 
+import org.rdorado.cheatsheet.core.POSTagger;
 import org.rdorado.cheatsheet.core.SenteceSegmenter;
+import org.rdorado.cheatsheet.core.TaggedSentence;
 
 import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation;
 import edu.stanford.nlp.pipeline.Annotation;
 import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import edu.stanford.nlp.tagger.maxent.MaxentTagger;
 import edu.stanford.nlp.util.CoreMap;
 
 //import edu.stanford.nlp.tagger.maxent.MaxentTagger;
 
-public class StanfordCoreNLPWrapper implements SenteceSegmenter{
+public class StanfordCoreNLPWrapper implements SenteceSegmenter, POSTagger{
 
 	StanfordCoreNLP pipeline;
 	
 	public StanfordCoreNLPWrapper() {
 		Properties props = new Properties();
-		props.put("annotators", "tokenize, ssplit"); //, pos, lemma, ner, parse, dcoref
+		props.put("annotators", "tokenize, ssplit, pos"); //, pos, lemma, ner, parse, dcoref
 		pipeline = new StanfordCoreNLP(props);
 	}
 
@@ -38,6 +41,19 @@ public class StanfordCoreNLPWrapper implements SenteceSegmenter{
 		return result;
 	}
 
+	@Override
+	public TaggedSentence tag(String sentence) {
+		MaxentTagger maxentTagger = new MaxentTagger("edu/stanford/nlp/models/pos-tagger/english-left3words/english-left3words-distsim.tagger");
+
+	    String tag = maxentTagger.tagString("He half closed his eyes and searched the horizon.");
+	    String[] eachTag = tag.split("\\s+");
+
+	    for(int i = 0; i< eachTag.length; i++) {
+	      System.out.print(eachTag[i].split("_")[0] +"_"+ eachTag[i].split("_")[1]+" ");
+	    }
+		return null;
+	}
+	
 	public static void main(String[] args){
 
 		/*MaxentTagger maxentTagger = new MaxentTagger("edu/stanford/nlp/models/pos-tagger/english-left3words/english-left3words-distsim.tagger");
@@ -48,7 +64,9 @@ public class StanfordCoreNLPWrapper implements SenteceSegmenter{
     for(int i = 0; i< eachTag.length; i++) {
       System.out.print(eachTag[i].split("_")[0] +"_"+ eachTag[i].split("_")[1]+" ");
     }*/
-
+		
+		StanfordCoreNLPWrapper coreNLPWrapper = new StanfordCoreNLPWrapper();
+		coreNLPWrapper.tag("The dog ate chocolate.");
 	}
 
 
